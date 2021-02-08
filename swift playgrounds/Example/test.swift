c16e02a0ff827fe9c45a755e9fdde2946f08505e
aman2025 <2247367564@qq.com>
2021-02-08 16:48:26 +0800

    修改文件目录

--- swift playgrounds/Example/test.swift
+++ swift playgrounds/Example/test.swift
@@ -1,1 +1,15 @@
-print("a1")
+
+import Foundation
+// struct LGTeacher  {
+// 	var age: Int
+	
+// }
+
+struct Person {
+	var name: String
+	var gender: String
+}
+let peoples = [Person(name: "1", gender: "male"), Person(name: "2", gender: "female")]
+
+
+print(peoples[0].name)
